d175d09d61ecb5588dd07b8b58ccd416707bb6c7
Marco Tiberio <45846191+marcotiberio@users.noreply.github.com>
2022-10-04 14:41:48 +0200

    style fixes

--- Components/HeroImageCta/functions.php
+++ Components/HeroImageCta/functions.php
@@ -47,24 +47,6 @@ function getACFLayout()
                 ]
             ],
             [
-                'label' => __('Header', 'flynt'),
-                'name' => 'header',
-                'type' => 'text',
-                'required' => 0,
-                'wrapper' => [
-                    'width' => 50
-                ],
-            ],
-            [
-                'label' => __('Sub Header', 'flynt'),
-                'name' => 'subHeader',
-                'type' => 'text',
-                'required' => 0,
-                'wrapper' => [
-                    'width' => 50
-                ],
-            ],
-            [
                 'label' => __('Content', 'flynt'),
                 'name' => 'contentHtml',
                 'type' => 'wysiwyg',
